73ce2dde57bf322053b898ab532437a6165e7ea3
david lau <david.kaimun.lau@gmail.com>
2024-05-29 10:24:11 +0200

    improvement: add style to map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,5 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 import mapboxgl from 'mapbox-gl'
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 
 // Connects to data-controller="map"
 export default class extends Controller {
@@ -13,13 +14,14 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/kailaulau/clp9syl68003m01o01p266rin"
     })
 
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
 
-    console.log(this.markersValue)
+    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl }))
   }
 
   #addMarkersToMap() {
